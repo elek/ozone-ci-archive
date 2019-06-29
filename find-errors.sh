d96b20d37f779febdcef755c99f3bb260fe272dc
Márton Elek <elek@apache.org>
2019-06-29 18:48:18 +0200

    delete logs of old successfull test runs

--- find-errors.sh
+++
@@ -1,8 +0,0 @@
-#/usr/bin/env bash
-
-for file in $(grep -r --include="*$1.txt" "Failures: 0, Errors: 0" -L); do
-   echo "FAILURE in $file"
-   cat "$file"
-   cat "${file%.*}-output.txt"
-   echo ""
-done
